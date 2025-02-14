ef2c4a7c38c13d2b2a2d5b20c2acc0f1d041009b
shiwanshbisht <shiwanshbisht@gmail.com>
2025-02-14 23:50:38 +0530

    Menu items localhost updated

--- fronted/src/components/Menuitem.js
+++ fronted/src/components/Menuitem.js
@@ -14,8 +14,9 @@ export const Menuitem = () => {
   const [debouncedSearch, setDebouncedSearch] = useState(search);
   const dispatch = useDispatch();
   const cartItems = useSelector((store) => store.cart.items);
-  const backendurl = process.env.REACT_APP_BACKEND_API_URL;
-
+  const backendurl = `${process.env.REACT_APP_BACKEND_API_URL}/menuitem`;
+  console.log("shiwansh", backendurl);
+  
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_API_URL}/menuitem`)
